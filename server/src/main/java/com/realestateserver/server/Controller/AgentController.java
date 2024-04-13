0ef7013925217f9bd3c47708fcd364ff725ad867
221026802 <221026802@tut4life.ac.za>
2024-04-13 09:55:34 +0200

    2nd  commit

---
+++ server/src/main/java/com/realestateserver/server/Controller/AgentController.java
@@ -0,0 +1,17 @@
+package com.realestateserver.server.Controller;
+
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@CrossOrigin
+@RequestMapping("api/v1/agent")
+
+public class AgentController {
+
+    /*@PostMapping(path = "/save")
+    public String saveAgent(@RequestBody AgentDTO agentDTO){
+        String id = agentService.addAgent(agentDTO);
+        return id;
+    }*/
+}
---
+++ server/src/main/java/com/realestateserver/server/Entity/Agent.java
@@ -0,0 +1,8 @@
+package com.realestateserver.server.Entity;
+//import jakarta.persistence.Entity;
+
+public class Agent {
+    private Long id;
+    private String name;
+    private String surname;
+}
